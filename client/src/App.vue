ddee61d9d47c6387c8b4ac4c7d636a8f29e95e63
Mateusz Kaczmarczyk <math.k999@gmail.com>
2019-03-26 23:39:36 +0100

    project initialization

---
+++ client/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <MenuBar/>
+  </div>
+</template>
+
+<script>
+import MenuBar from './components/MenuBar'
+
+export default {
+  name: 'App',
+  components: {
+    MenuBar
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 25px;
+  margin-left: 75px;
+  margin-right: 75px;
+}
+</style>
---
+++ client/src/components/MenuBar.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <b-navbar>
+      <b-navbar-brand href="#"><img src=".././assets/menu-logo.png"></b-navbar-brand>
+
+      <b-navbar-nav class="ml-auto pt-4">
+        <b-nav-item class="mr-5" href="#">Oferta</b-nav-item>
+        <b-nav-item class="mr-5" href="#">Aktualności</b-nav-item>
+        <b-nav-item class="mr-5" href="#">O nas</b-nav-item>
+        <b-nav-item class="mr-5" href="#">Kontakt</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  @import url('https://fonts.googleapis.com/css?family=Roboto');
+
+  div {
+    font-family: 'Roboto', Helvetica, Arial, sans-serif;
+    font-weight: 100;
+    font-size: 25px;
+  }
+</style>
